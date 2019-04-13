8226a99d7c317b0fa57b372edb8a5eaad3bef095
Lucas Hild <contact@lucas-hild.de>
2019-04-13 18:52:35 +0200

    Restructure js files

---
+++ static/balance.js
@@ -0,0 +1,28 @@
+export function balance() {
+    let balanceLoadingElement = document.getElementById('balance-loading')
+    let balanceContentElement = document.getElementById('balance-content')
+    let balanceAmountElement = document.getElementById('balance-amount')
+    let expectedBalanceAmountElement = document.getElementById('expected-balance-amount')
+
+    balanceLoadingElement.style.display = 'block'
+
+    fetch('/api/balance')
+        .then(response => response.json())
+        .then(data => {
+            balanceAmountElement.innerText = data.balance + ' €'
+            if (data.balance < 0) {
+                balanceAmountElement.classList.add('has-text-danger')
+            }
+
+            expectedBalanceAmountElement.innerText = data.expectedBalance + ' €'
+            if (data.expectedBalance < 0) {
+                expectedBalanceAmountElement.classList.add('has-text-danger')
+            }
+
+            balanceLoadingElement.style.display = 'none'
+            balanceContentElement.style.display = 'block'
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
