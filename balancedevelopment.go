51cb746f0a7a8b28a87daec8f1e20607808712c7
Lucas Hild <contact@lucas-hild.de>
2019-04-13 21:04:21 +0200

    Add labels to each month for balance development

--- balancedevelopment.go
+++ balancedevelopment.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"sort"
+	"strconv"
 	"time"
 )
 
 type Month struct {
-	Date    string  `json:"date"`
-	Label   string  `json:"label"`
-	Balance float64 `json:"balance"`
+	Date       time.Time `json:"-"`
+	DateString string    `json:"date"`
+	Label      string    `json:"label"`
+	Balance    float64   `json:"balance"`
 }
 
 func calculateBalanceDevelopment(receipts []Receipt, invoices []Invoice) ([]Month, error) {
 	months := getEmptyMonths()
+	months = addLabelsToMonths(months)
 	return months, nil
 }
 
@@ -24,7 +27,8 @@ func getEmptyMonths() []Month {
 
 	for index := 0; index < 12; index++ {
 		months = append(months, Month{
-			Date: currentMonth.Format("02.01.2006"),
+			Date:       currentMonth,
+			DateString: currentMonth.Format("02.01.2006"),
 		})
 
 		currentMonth = currentMonth.AddDate(0, -1, 0)
@@ -34,6 +38,18 @@ func getEmptyMonths() []Month {
 	sort.SliceStable(months, func(i, j int) bool {
 		return true
 	})
+
+	return months
+}
+
+// addLabelsToMonths adds a label consisting of the month of the year of each month
+// e. g. Mar 19
+func addLabelsToMonths(months []Month) []Month {
+	for index, month := range months {
+		monthName := month.Date.Month().String()[:3]
+		year := strconv.Itoa(month.Date.Year())[2:]
+		months[index].Label = monthName + " " + year
+	}
 
 	return months
 }
