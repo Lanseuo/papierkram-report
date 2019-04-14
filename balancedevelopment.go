6d6a731762a5f91777ce07c0adf35bfc47254ef5
Lucas Hild <contact@lucas-hild.de>
2019-04-14 10:55:05 +0200

    Add logic for balance development

--- balancedevelopment.go
+++ balancedevelopment.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"sort"
 	"strconv"
 	"time"
@@ -16,6 +17,25 @@ type Month struct {
 func calculateBalanceDevelopment(receipts []Receipt, invoices []Invoice) ([]Month, error) {
 	months := getEmptyMonths()
 	months = addLabelsToMonths(months)
+
+	for monthIndex, month := range months {
+		balanceAfterMonth := 0.0
+
+		for _, receipt := range receipts {
+			if paymentDateWasBeforeEndOfMonth(receipt.PaymentDate, month.Date) {
+				balanceAfterMonth += receipt.NetAmount
+			}
+		}
+
+		for _, invoice := range invoices {
+			if paymentDateWasBeforeEndOfMonth(invoice.LastPaymentDate, month.Date) {
+				balanceAfterMonth += invoice.NetAmount
+			}
+		}
+
+		months[monthIndex].Balance = balanceAfterMonth
+	}
+
 	return months, nil
 }
 
@@ -52,4 +72,20 @@ func addLabelsToMonths(months []Month) []Month {
 	}
 
 	return months
+}
+
+func paymentDateWasBeforeEndOfMonth(paymentDate string, monthDate time.Time) bool {
+	if paymentDate == "" {
+		return false
+	}
+
+	paymentMonth, monthErr := strconv.Atoi(paymentDate[3:5])
+	paymentYear, yearErr := strconv.Atoi(paymentDate[6:])
+	if monthErr != nil || yearErr != nil {
+		log.Fatalln("Unable to parse date from receipt", monthErr, yearErr)
+	}
+
+	isInYearsBefore := paymentYear < monthDate.Year()
+	isInSameYearButBefore := paymentYear == monthDate.Year() && paymentMonth <= int(monthDate.Month())
+	return isInYearsBefore || isInSameYearButBefore
 }
